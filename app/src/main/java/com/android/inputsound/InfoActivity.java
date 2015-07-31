c5842868b7e6b160fe34875acc81f08cbfdc660c
jeongseunghyeon <jsh152169@gmail.com>
2015-07-31 15:08:32 +0900

    상단 메뉴 Information 추

---
+++ app/src/main/java/com/android/inputsound/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.android.inputsound;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jeong on 15. 7. 10.
+ */
+public class InfoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information);
+        //ActionBar actionbar = getActionBar();
+        //actionbar.hide();
+    }
+}
